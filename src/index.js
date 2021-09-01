ec0b53aa502c781bbecb8e0bedf6613307db0339
medaminedev66 <md.amine.smahi@gmail.com>
2021-09-01 17:05:04 +0100

    Send and receive data from an API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
+import { create, forEach } from 'lodash';
 import './style.css';
+
+const scores = document.querySelector('.scores > ul');
+const getAPIData = () => {
+  async function getFetch() {
+    let response = await fetch(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/9I754jQkwVXa66yLA2rE/scores',
+    );
+    if (!response.ok) {
+      throw new Error(`API error! status: ${response.status}`);
+    }
+    return await response.json();
+  }
+  getFetch()
+    .then((data) => {
+      //   console.log(data.result[0]);
+      data.result.forEach((player) => {
+        console.log(player);
+        const li = document.createElement('li');
+        const playerName = document.createElement('p');
+        const playerScore = document.createElement('p');
+        li.appendChild(playerName);
+        li.appendChild(playerScore);
+        scores.appendChild(li);
+        playerName.innerText = player.user;
+        playerScore.innerText = player.score;
+      });
+    })
+    .catch((e) => console.log(e));
+};
+document.querySelector('.add').addEventListener('click', () => {
+  const nameInput = document.querySelector('.name').value;
+  const scoreInput = document.querySelector('.score').value;
+  console.log(nameInput, scoreInput);
+  async function saveFetch() {
+    let response = await fetch(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/9I754jQkwVXa66yLA2rE/scores',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          user: nameInput,
+          score: scoreInput,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      },
+    );
+    if (!response.ok) {
+      throw new Error(`API error! status: ${response.status}`);
+    }
+    return await response.json();
+  }
+  saveFetch().then((json) => console.log(json));
+  document.querySelector('.name').value = '';
+  document.querySelector('.score').value = '';
+});
+
+document.querySelector('.refresh').addEventListener('click', () => {
+  scores.innerHTML = '';
+  getAPIData();
+});
+
+window.onload = getAPIData();
